9824f2fbdc01d247da5087293e94abad8290a005
Forbzz <vital77717@gmail.com>
2022-04-01 16:27:47 +0300

    Doctor & Pacient controllers

---
+++ App/Controllers/DoctorsController.cs
@@ -0,0 +1,124 @@
+﻿using App.ViewModels.Doctors;
+using Data.Domain;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Repository.Interface;
+using App.ViewModels.User;
+using App.ViewModels.Stats;
+
+namespace App.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+        private readonly IDoctorService _doctorService;
+        private readonly IStatsService _statsService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public DoctorsController(ILogger<UsersController> logger, IDoctorService doctorService, IStatsService statsService, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            _doctorService = doctorService;
+            _statsService = statsService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new DoctorSearchViewModel();
+            model.Branches = await _doctorService.GetBranches().Select(x => x.Name).ToListAsync();
+            model.Specialities = await _doctorService.GetSpecialities().Select(x => x.Name).ToListAsync();
+            model.Doctors = await _doctorService.GetDoctors().ToListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(DoctorSearchViewModel model)
+        {
+            model.Branches = await _doctorService.GetBranches().Select(x => x.Name).ToListAsync();
+            model.Specialities = await _doctorService.GetSpecialities().Select(x => x.Name).ToListAsync();
+            model.Doctors = await _doctorService.GetDoctors(model.Name, model.Speciality, model.Branch).ToListAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(string id, string returnUrl)
+        {
+            var doc = await _doctorService.GetDoctorByIdAsync(id);
+            var model = new DetailsDoctorViewModel
+            {
+                Doctor = doc,
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id, string returnUrl)
+        {
+            var model = new DoctorEditViewModel();
+            model.ReturnUrl = returnUrl;
+            var doc = await _doctorService.GetDoctorByIdAsync(id);
+            var specs = await _doctorService.GetSpecialities()
+                    .Select(x => x.Name).ToListAsync();
+            var branches = await _doctorService.GetBranches()
+                .Select(x => x.Name).ToListAsync();
+
+            model.EditModel = new EditUserViewModel(doc, "Doctor", branches, specs);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(DoctorEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var docModel = model.EditModel;
+            await _doctorService.UpdateAsync(docModel.Id, docModel.Name1, docModel.Name2,
+                    docModel.Name3, docModel.Branch, docModel.Speciality, docModel.ContactEmail,
+                    docModel.ContactPhoneNumber, docModel.CabinetNumber);
+
+            if (!string.IsNullOrEmpty(model.ReturnUrl))
+            {
+                return LocalRedirect(model.ReturnUrl);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Stats(string id, string returnUrl)
+        {
+            if (string.IsNullOrEmpty(id))
+                id = _userManager.GetUserId(User);
+            var now = DateTime.Now;
+
+            var model = new DoctorStatsViewModel()
+            {
+                Start = new DateTime(now.Year, now.Month, 1),
+                End = now.Date,
+                ReturnUrl = returnUrl
+            };
+
+            model.Stats = await _statsService.GetDoctorStats(id, model.Start, model.End);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Stats(DoctorStatsViewModel model)
+        {
+            model.Stats = await _statsService.GetDoctorStats(model.Id, model.Start, model.End);
+            return View(model);
+        }
+
+
+    }
+}
---
+++ App/Controllers/PacientsController.cs
@@ -0,0 +1,128 @@
+﻿using App.ViewModels;
+using App.ViewModels.Pacients;
+using Data.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interface;
+using System;
+using System.Threading.Tasks;
+
+namespace App.Controllers
+{
+    public class PacientsController : Controller
+    {
+        private readonly IPacientService _pacientService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public PacientsController(IPacientService pacientService,
+            UserManager<AppUser> userManager)
+        {
+            _pacientService = pacientService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(int? page)
+        {
+            page ??= 1;
+            var model = new PacientIndexViewModel();
+            var count = await _pacientService.GetPacients().CountAsync();
+            model.PageViewModel = new PageViewModel(page.Value, 10, count);
+            var skip = (page.Value - 1) * model.PageViewModel.PageSize;
+
+            if (skip > count)
+            {
+                return NotFound();
+            }
+
+            model.PageViewModel = new PageViewModel(page.Value, 10, count);
+
+            model.Pacients = _pacientService.GetPacients(skip, model.PageViewModel.PageSize, includeCard: true, includeAddress: true);
+            model.Search = new PacientSearchViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(int? page, PacientSearchViewModel model)
+        {
+            page ??= 1;
+            var pacientModel = new PacientIndexViewModel { Search = model };
+
+            var query = _pacientService.SearchPacient(model.Name,
+                model.CardNumber, model.Address);
+
+            var count = await query.CountAsync();
+            var pvm = new PageViewModel(page.Value, 10, count);
+
+            var skip = (page.Value - 1) * pvm.PageSize;
+            query = _pacientService.SearchPacient(model.Name,
+                model.CardNumber, model.Address, skip, pvm.PageSize, includeCard: true,
+                includeAddress: true, includeContacts: true);
+
+            if (skip > count)
+            {
+                return NotFound();
+            }
+
+            return View("Index", new PacientIndexViewModel
+            {
+                Pacients = await query.ToListAsync(),
+                PageViewModel = pvm,
+                Search = model
+            });
+        }
+
+        public async Task<IActionResult> Details(string id, string returnUrl)
+        {
+            var pacient = await _pacientService.GetPacientByIdAsync(id, true, true, false);
+            pacient.Address = await _pacientService.GetPacientAddress(id);
+
+            var model = new PacientInfoViewModel
+            {
+                Pacient = pacient,
+                returnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id, string returnUrl)
+        {
+            var model = new PacientInfoViewModel
+            {
+                Pacient = await _pacientService.GetPacientByIdAsync(id, true, false, true),
+                returnUrl = returnUrl
+            };
+
+            if(model.Pacient.BirthDay == null)
+            {
+                model.Pacient.BirthDay = DateTime.Now.Date;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PacientEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                var res = await _pacientService.UpdateAsync(model.Pacient);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(model);
+            }
+
+            return LocalRedirect(model.ReturnUrl);
+        }
+
+    }
+}
